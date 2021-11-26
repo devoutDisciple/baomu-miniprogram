c1149dea168857cc1c0d34960e5106c03d2803fd
张振 <1094705507@qq.com>
2021-11-27 00:09:05 +0800

    feat: 修改我的页面

--- pages/my/index/index.js
+++ pages/my/index/index.js
@@ -5,7 +5,9 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-		statusHeight: '80px',
+		statusHeight: '80px', // 总高度
+		statusBarHeight: '40px', // 上方状态栏高度
+		showLoading: false, // 加载图标
 	},
 
 	/**
@@ -23,16 +25,24 @@ Page({
 	/**
 	 * 页面相关事件处理函数--监听用户下拉动作
 	 */
-	onPullDownRefresh: function () {},
+	onPullDownRefresh: function () {
+		this.setData({ showLoading: true });
+		setTimeout(() => {
+			wx.stopPullDownRefresh({
+				success: () => {
+					this.setData({ showLoading: false });
+				},
+			});
+		}, 500);
+	},
 
-	// 获取设备信息
 	// 获取设备信息
 	getDeviceData: function () {
 		// 获取设备信息
 		deviceUtil.getDeviceInfo().then((res) => {
 			const { navHeight, statusBarHeight } = res;
-			console.log(res, 2332);
 			this.setData({
+				statusBarHeight: `${statusBarHeight}px`,
 				statusHeight: `${navHeight + statusBarHeight}px`,
 			});
 		});
