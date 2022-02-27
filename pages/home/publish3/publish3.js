2129886e68fce45d705a81e05c6e68ec5999a67b
张振 <1094705507@qq.com>
2022-02-27 23:46:13 +0800

    修改发布

--- pages/home/publish3/publish3.js
+++ pages/home/publish3/publish3.js
@@ -1,66 +1,56 @@
-// pages/home/publish3/publish3.js
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		typeActions: ['小提琴演奏', '钢琴演奏'],
+		typeSelect: '',
+	},
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
 
-  },
+	bindPickerChange: function (e) {
+		const { value } = e.detail;
+		const typeSelect = this.data.typeActions[value];
+		console.log(typeSelect);
+		this.setData({ typeSelect: typeSelect });
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
 
-  },
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
 
-  },
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
 
-  },
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+});
