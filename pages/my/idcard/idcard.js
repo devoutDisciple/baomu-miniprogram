74e0056459b366ab54a69dcc15859eb8876c1108
张振 <1094705507@qq.com>
2022-03-16 23:51:05 +0800

    完成实名认证

--- pages/my/idcard/idcard.js
+++ pages/my/idcard/idcard.js
@@ -1,4 +1,6 @@
-// pages/my/idcard/idcard.js
+import request, { uploadFile } from '../../../utils/request';
+import loading from '../../../utils/loading';
+
 Page({
 	/**
 	 * 页面的初始数据
@@ -6,7 +8,7 @@ Page({
 	data: {
 		idcard1: '', // 身份证正面
 		idcard2: '', // 反面
-		type: 2, // 1-未认证 2-认证中 3-认证失败 4-认证成功
+		type: 1, // 1-未认证 2-认证中 3-认证失败 4-认证成功
 		dialogShow: false,
 		dialogDetail: {
 			title: '已提交审核!',
@@ -18,7 +20,24 @@ Page({
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
-	onLoad: function () {},
+	onLoad: function () {
+		this.getUserState();
+	},
+
+	// 获取用户是否认证装填
+	getUserState: async function () {
+		loading.showLoading();
+		const user_id = wx.getStorageSync('user_id');
+		const result = await request.get({ url: '/idcard/all', data: { user_id } });
+		if (result.state && result.idcard1) {
+			this.setData({
+				idcard1: result.idcard1,
+				idcard2: result.idcard2,
+				type: result.state,
+			});
+		}
+		loading.hideLoading();
+	},
 
 	// 选择照片
 	onChooseImg: function (e) {
@@ -47,8 +66,26 @@ Page({
 	},
 
 	// 提交审核
-	onSend: function () {
-		this.setData({ dialogShow: true });
+	onSend: async function () {
+		const { idcard1, idcard2 } = this.data;
+		const user_id = wx.getStorageSync('user_id');
+		if (!idcard1 || !idcard2) {
+			return wx.showToast({
+				title: '请完善信息',
+				icon: 'error',
+			});
+		}
+		loading.showLoading();
+		const idcard1Img = await uploadFile({ url: '/idcard/upload', data: idcard1 });
+		const idcard2Img = await uploadFile({ url: '/idcard/upload', data: idcard2 });
+		const result = await request.post({
+			url: '/idcard/add',
+			data: { user_id, idcard1: idcard1Img.url, idcard2: idcard2Img.url },
+		});
+		loading.hideLoading();
+		if (result === 'success') {
+			this.setData({ dialogShow: true });
+		}
 	},
 
 	// 关闭弹框
